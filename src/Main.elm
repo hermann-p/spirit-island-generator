b2ad66e5603d68935cb520333d7c5296695452eb
Hermann Pauly <hp@campudus.com>
2023-07-31 16:20:42 +0200

    Add game sets to spirits

--- src/Main.elm
+++ src/Main.elm
@@ -2,32 +2,47 @@ module Main exposing (..)
 
 import Browser
 import Dict exposing (update)
-import Element exposing (Element, centerX, column, el, fill, image, layout, padding, px, rgb, spacing, text, width)
-import Element.Input as Input exposing (button, labelAbove)
-import Html.Attributes exposing (src)
-import Html.Events exposing (onClick)
-import MC
+import Element exposing (Element, centerX, column, el, fill, image, layout, padding, paddingXY, row, spacing, text, width)
+import Element.Input exposing (button, labelAbove)
 import Random exposing (generate)
 import Random.List exposing (choices, choose)
 import Spirits exposing (Spirit, spirits)
 import String exposing (fromInt)
+import Tools exposing (splitListFull)
 import UI
+
+
+maxPlayers : Int
+maxPlayers =
+    6
 
 
 type alias Model =
     { availableSpirits : List Spirit
+    , chosenSpirits : List Spirit
     , nPlayers : Int
     , players : List Spirit
+    , currentView : View
     }
 
 
 initial : Model
 initial =
-    { nPlayers = 2, availableSpirits = spirits, players = [] }
+    { nPlayers = 2
+    , availableSpirits = spirits
+    , players = []
+    , currentView = MainView
+    , chosenSpirits = []
+    }
 
 
 type alias Flags =
     {}
+
+
+type View
+    = MainView
+    | SettingsView
 
 
 init : Flags -> ( Model, Cmd Msg )
@@ -48,12 +63,12 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         SetPlayers n ->
-            update RollAllSpirits { model | nPlayers = n }
+            ( { model | nPlayers = n }, Cmd.none )
 
         RollAllSpirits ->
             let
                 spiritGenerator =
-                    choices model.nPlayers spirits
+                    choices maxPlayers model.availableSpirits
             in
             ( model, generate SetAllSpirits spiritGenerator )
 
@@ -67,7 +82,7 @@ update msg model =
         SetAllSpirits ( chosen, left ) ->
             ( { model | players = chosen, availableSpirits = left }, Cmd.none )
 
-        SetSpirit n ( Just chosen, left ) ->
+        SetSpirit n ( Just chosen, _ ) ->
             let
                 players =
                     List.indexedMap
@@ -89,12 +104,49 @@ update msg model =
             ( model, Cmd.none )
 
 
+settingsView : model -> List (Element Msg)
+settingsView _ =
+    List.map spiritToggle spirits
+
+
+toGrid : Int -> (a -> Element msg) -> List a -> Element msg
+toGrid cols toElement xs =
+    if List.length xs < 2 then
+        xs |> List.head |> Maybe.map toElement |> Maybe.withDefault (text "")
+
+    else
+        xs
+            |> List.map toElement
+            |> splitListFull cols (column [ width fill ] [ text "" ])
+            |> List.map (row [ width fill, spacing 16, paddingXY 0 8 ])
+            |> column [ width fill ]
+
+
+selectList : { selected : List Spirit, options : List Spirit, onCheck : Bool -> Spirit -> msg } -> Element msg
+selectList cfg =
+    let
+        selectElement : Spirit -> Element msg
+        selectElement el =
+            let
+                checked =
+                    List.member el cfg.selected
+            in
+            button [] { onPress = Just <| cfg.onCheck (not checked) el, label = image [ width fill ] { src = spiritToImageUrl el, description = el.name } }
+    in
+    toGrid 2 selectElement cfg.options
+
+
+spiritToggle : Spirit -> Element Msg
+spiritToggle spirit =
+    image [] { description = spirit.name, src = spiritToImageUrl spirit }
+
+
 selectPlayersView : Model -> Element Msg
 selectPlayersView model =
     UI.intSlider
         { label = labelAbove [] <| el [ centerX ] (text <| "Spieler: " ++ fromInt model.nPlayers)
         , min = 1
-        , max = 5
+        , max = maxPlayers
         , onChange = SetPlayers
         , step = 1
         , value = model.nPlayers
@@ -114,17 +166,30 @@ playerView idx spirit =
         }
 
 
+mainView : Model -> List (Element Msg)
+mainView model =
+    [ selectPlayersView model
+    , UI.button { onPress = RollAllSpirits, label = el [ centerX ] (text "Geister neu würfeln"), disabled = False }
+    , List.take model.nPlayers model.players |> List.indexedMap playerView |> toGrid 2 identity
+    ]
+
+
 view : Model -> Browser.Document Msg
 view model =
+    let
+        content =
+            case model.currentView of
+                MainView ->
+                    mainView
+
+                SettingsView ->
+                    settingsView
+    in
     { title = "Spirit Island game generator"
     , body =
-        [ layout [ padding 32 ]
+        [ layout [ padding 16 ]
             (column [ width fill, spacing 32 ]
-                ([ selectPlayersView model
-                 , UI.button { onPress = RollAllSpirits, label = el [ centerX ] (text "Geister neu würfeln"), disabled = False }
-                 ]
-                    ++ List.indexedMap playerView model.players
-                )
+                (content model)
             )
         ]
     }
--- src/Spirits.elm
+++ src/Spirits.elm
@@ -8,43 +8,54 @@ type Complexity
     | VeryHigh
 
 
+type GameSet
+    = Base
+    | JaggedEarth
+    | BranchAndPaw
+    | Promo1
+    | Promo2
+    | FeatherAndFlame
+    | NatureIncarnate
+    | Horizons
+
+
 type alias Spirit =
     { name : String
     , complexity : Complexity
+    , origin : GameSet
     }
 
 
 spirits : List Spirit
 spirits =
-    [ { name = "Lightning's Swift Strike", complexity = Low }
-    , { name = "River Surges in Sunlight", complexity = Low }
-    , { name = "Vital Strength of the Earth", complexity = Low }
-    , { name = "Shadows Flicker Like Flame", complexity = Low }
-    , { name = "Thunderspeaker", complexity = Med }
-    , { name = "A Spread of Rampant Green", complexity = Med }
-    , { name = "Ocean's Hungry Grasp", complexity = High }
-    , { name = "Bringer of Dreams and Nightmares", complexity = High }
-    , { name = "Sharp Fangs Behind the Leaves", complexity = Med }
-    , { name = "Keeper of the Forbidden Wilds", complexity = Med }
+    [ { name = "Lightning's Swift Strike", complexity = Low, origin = Base }
+    , { name = "River Surges in Sunlight", complexity = Low, origin = Base }
+    , { name = "Vital Strength of the Earth", complexity = Low, origin = Base }
+    , { name = "Shadows Flicker Like Flame", complexity = Low, origin = Base }
+    , { name = "Thunderspeaker", complexity = Med, origin = Base }
+    , { name = "A Spread of Rampant Green", complexity = Med, origin = Base }
+    , { name = "Ocean's Hungry Grasp", complexity = High, origin = Base }
+    , { name = "Bringer of Dreams and Nightmares", complexity = High, origin = Base }
+    , { name = "Sharp Fangs Behind the Leaves", complexity = Med, origin = Base }
+    , { name = "Keeper of the Forbidden Wilds", complexity = Med, origin = Base }
+    , { name = "Heart of the Wildfire", complexity = High, origin = Promo1 }
+    , { name = "Serpent Slumbering Beneath the Island", complexity = High, origin = Promo1 }
+    , { name = "Stone's Unyielding Defiance", complexity = Med, origin = JaggedEarth }
+    , { name = "Shifting Memory of Ages", complexity = Med, origin = JaggedEarth }
+    , { name = "Grinning Trickster Stirs Up Trouble", complexity = Med, origin = JaggedEarth }
+    , { name = "Lure of the Deep Wilderness", complexity = Med, origin = JaggedEarth }
+    , { name = "Many Minds Move as One", complexity = Med, origin = JaggedEarth }
+    , { name = "Volcano Looming High", complexity = Med, origin = JaggedEarth }
+    , { name = "Shroud of Silent Mist", complexity = High, origin = JaggedEarth }
+    , { name = "Vengeance as a Burning Plague", complexity = High, origin = JaggedEarth }
+    , { name = "Starlight Seeks Its Form", complexity = VeryHigh, origin = JaggedEarth }
+    , { name = "Fractured Days Split the Sky", complexity = VeryHigh, origin = JaggedEarth }
+    , { name = "Downpour Drenches the World", complexity = High, origin = Promo2 }
+    , { name = "Finder of Paths Unseen", complexity = VeryHigh, origin = Promo2 }
 
-    -- , { name = "Heart of the Wildfier", complexity = High }
-    -- , { name = "Serpent Slumbering Beneath the Island", complexity = High }
-    , { name = "Stone's Unyielding Defiance", complexity = Med }
-    , { name = "Shifting Memory of Ages", complexity = Med }
-    , { name = "Grinning Trickster Stirs Up Trouble", complexity = Med }
-    , { name = "Lure of the Deep Wilderness", complexity = Med }
-    , { name = "Many Minds Move as One", complexity = Med }
-    , { name = "Volcano Looming High", complexity = Med }
-    , { name = "Shroud of Silent Mist", complexity = High }
-    , { name = "Vengeance as a Burning Plague", complexity = High }
-    , { name = "Starlight Seeks Its Form", complexity = VeryHigh }
-    , { name = "Fractured Days Split the Sky", complexity = VeryHigh }
-    , { name = "Downpour Drenches the World", complexity = High }
-
-    -- , { name = "Finder of Paths Unseen", complexity = VeryHigh }
-    -- , { name = "Devouring Teeth Lurk Underfoot", complexity = Low }
-    -- , { name = "Eyes Watch From the Trees", complexity = Low }
-    -- , { name = "Fathomless Mud of the Swamp", complexity = Low }
-    -- , { name = "Rising Head of Stone and Sand", complexity = Low }
-    -- , { name = "Sun-Bright Whirlwind", complexity = Low }
+    -- , { name = "Devouring Teeth Lurk Underfoot", complexity = Low, origin = Horizons }
+    -- , { name = "Eyes Watch From the Trees", complexity = Low, origin = Horizons }
+    -- , { name = "Fathomless Mud of the Swamp", complexity = Low, origin = Horizons }
+    -- , { name = "Rising Head of Stone and Sand", complexity = Low , origin = Horizons}
+    -- , { name = "Sun-Bright Whirlwind", complexity = Low , origin = Horizons}
     ]
